cdee7cb375f8608a83153d1563140121db694360
programming <e.babyle206@gmail.com>
2024-03-11 21:34:03 +0530

    add files' cls

---
+++ main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render,HttpResponse 
+import wikipedia
+# Create your views here.
+def home(request):
+    if request.method=='POST':
+        search=request.POST['search']
+        try:
+            result=wikipedia.summary(search,sentences=3)
+        except:
+            return HttpResponse('Wrong input')
+        return render(request,'main/index.html',{'result':result})
+    return render(request,'main/index.html')
